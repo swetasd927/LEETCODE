a631b92d3829956cd477b48f489cb49fd5649622
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-02 09:55:37 +0545

    Remove elements

---
+++ DAY2/RemoveElements.js
@@ -0,0 +1,13 @@
+var removeElement = (nums, val) => {
+    let i=0;
+    for(let j=0; j<nums.length; j++){
+        if(nums[j] !==val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
+
+//TIME COMPLEXITY: O(n)
+//Space complexity: O(1)
