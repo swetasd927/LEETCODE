fc2e83e9fcc069eb9717c7a5abb1ecf6c018cf98
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-09 22:00:28 +0545

    Construct binary tree from inorder and preorder travesral

--- DAY9/ConstructBinaryTreeFromPreorderAndInorderTraversal.js
+++ DAY9/ConstructBinaryTreeFromPreorderAndInorderTraversal.js
@@ -1,6 +1,43 @@
 //https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=problem-list-v2&envId=rab78cw1
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
 
+function buildTree(preorder, inorder) {
+    let map = new Map();
+    for (let i = 0; i < inorder.length; i++) {
+        map.set(inorder[i], i);
+    }
+
+    let preIndex = 0;
+
+    function helper(left, right) {
+        if (left > right) return null;
+
+        let rootVal = preorder[preIndex++];
+        let root = new TreeNode(rootVal);
+
+        let index = map.get(rootVal);
+
+        root.left = helper(left, index - 1);
+        root.right = helper(index + 1, right);
+
+        return root;
+    }
+
+    return helper(0, inorder.length - 1);
+}
 
 //Take root from preorder, find it in inorder to divide left and right parts, then recursively build both sides.
 //time and space: O(n)
