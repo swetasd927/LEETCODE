192366030a64b53c738a859fa2cdef2a443ad1d2
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-11 13:45:07 +0545

    Word search

---
+++ WEEK2/DAY11/WordSearch.js
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/course-schedule/submissions/1975109406/?envType=problem-list-v2&envId=rab78cw1
+//Array, String, Backtracking, DFS, Matrix
+
+//Try every cell as starting point
+//Run DFS to match characters step by step
+//Mark visited cell temporarily
+//Backtrack if path fails
