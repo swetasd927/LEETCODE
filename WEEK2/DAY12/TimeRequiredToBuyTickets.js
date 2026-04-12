705f49b31f4f8c85c16a2da0ec156656b9d496ac
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-12 12:33:20 +0545

    Time required to buy tickets

---
+++ WEEK2/DAY12/TimeRequiredToBuyTickets.js
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/time-needed-to-buy-tickets/submissions/1976147933/
+//Mid, Array, Queue, Simulation
+
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+    let time = 0;
+    for(let i = 0; i<tickets.length; i++){
+        if(i<=k){
+            time += Math.min(tickets[i], tickets[k]); 
+        } else {
+            time += Math.min(tickets[i], tickets[k] -1);
+        }
+    }
+    return time;
+};
+
+//time: O(n) : single loop
+//space: O(1) : no extra space
