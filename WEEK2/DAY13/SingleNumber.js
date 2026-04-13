33e91c625a1a0e9b293e554a9481c5f5223efaed
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-13 21:21:14 +0545

    Single num

--- WEEK2/DAY13/SingleNumber.js
+++ WEEK2/DAY13/SingleNumber.js
@@ -1,37 +1,20 @@
-//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/?envType=problem-list-v2&envId=array
+//https://leetcode.com/problems/single-number/?envType=problem-list-v2&envId=array
 
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
 /**
  * @param {number[]} nums
- * @return {TreeNode}
+ * @return {number}
  */
 
-var sortedArrayToBST = function(nums) {
-    
-    function build(left, right) {
-        if (left > right) return null;
 
-        let mid = Math.floor((left + right) / 2);
-        let root = new TreeNode(nums[mid]);
+var singleNumber = function(nums) {
+    let result = 0;
 
-        root.left = build(left, mid - 1);
-        root.right = build(mid + 1, right);
-
-        return root;
+    for (let num of nums) {
+        result ^= num;
     }
 
-    return build(0, nums.length - 1);
+    return result;
 };
 
-//Using divide and conquer, I pick middle as root to keep BST height-balanced.
-
-//Time: O(n) (visit each node once)
-//Space: O(log n) (recursion stack)
+//i used  XOR since duplicates cancel out and only the unique element remains.
+//Time: O(n), Space: O(1) 
