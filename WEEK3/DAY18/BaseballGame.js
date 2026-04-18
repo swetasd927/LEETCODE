9bbb6e952b147fd95f1d4e43772752d3555c81b8
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-18 09:32:52 +0545

    opt baseball games

--- WEEK3/DAY18/BaseballGame.js
+++ WEEK3/DAY18/BaseballGame.js
@@ -32,3 +32,51 @@ var calPoints = function(ops) {
 };
 
 //to optimise it
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(ops) {
+    let stack = [];
+    let sum = 0;
+    for(let op of ops) {
+        if(op === '+'){
+            let val = stack[stack.length -1] + stack[stack.length -2];
+            stack.push(val);
+            sum += val;
+        } else if (op === 'D') {
+            let val =2 * stack[stack.length -1];    
+            stack.push(val);
+            sum += val;
+        } else if (op ==='C') {
+            sum -= stack.pop();
+        }else{
+            let val =(Number(op));
+            stack.push(val);
+            sum += val;
+        }
+    }
+    //sum all values
+    return sum;
+};
+
+/**
+ * I’ll use a stack to simulate the score record. 
+ * Each operation modifies the stack:
+
+If it’s a number then  push it
+‘+’ push sum of last two elements
+‘D’ push double of last element
+‘C’ pop the last element
+
+Additionally, instead of calculating the sum at the end, 
+I maintain a running total while processing each operation. 
+This avoids an extra pass over the data.
+ */
+
+/**
+ * By maintaining a running sum, I eliminate the need for a second traversal to 
+ * compute the total, reducing constant overhead while keeping the same O(n) complexity.
+ * Time and Space: O(n)
+ */
