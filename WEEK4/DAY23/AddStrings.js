66820ccd68223c96f816066ac68b8d06532b0d42
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-23 21:39:44 +0545

    Add strings

--- WEEK4/DAY23/AddStrings.js
+++ WEEK4/DAY23/AddStrings.js
@@ -1,5 +1,28 @@
 //https://leetcode.com/problems/add-strings/description/?envType=problem-list-v2&envId=string
 
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1, j = num2.length - 1;
+    let carry = 0, res = "";
+
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry;
+
+        if (i >= 0) sum += num1.charCodeAt(i--) - 48;
+        if (j >= 0) sum += num2.charCodeAt(j--) - 48;
+
+        res = (sum % 10) + res;
+        carry = Math.floor(sum / 10);
+    }
+
+    return res;
+};
+
 //Traverse both strings from right to left, add digits with carry, and build the result string.
 //Time: O(max(n, m))
 //Space: O(n)
