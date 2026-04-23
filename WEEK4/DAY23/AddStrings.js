92020588c081153d61c1ab52465ff4288a0fd0a8
Sweta Dahal <dahalsweta5@gmail.com>
2026-04-23 21:39:25 +0545

    Add string qtn

---
+++ WEEK4/DAY23/AddStrings.js
@@ -0,0 +1,5 @@
+//https://leetcode.com/problems/add-strings/description/?envType=problem-list-v2&envId=string
+
+//Traverse both strings from right to left, add digits with carry, and build the result string.
+//Time: O(max(n, m))
+//Space: O(n)
